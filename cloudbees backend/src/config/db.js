1ba4287b8f577321e8ab84333e63909810d645a1
ArthiH <arthihari398@gmail.com>
2023-11-29 17:12:51 +0530

    added at files 29/11/23

--- cloudbees backend/src/config/db.js
+++ cloudbees backend/src/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect("mongodb://0.0.0.0:27017/", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.cv4hyu6.mongodb.net/`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -14,5 +17,3 @@ mongoose
   });
 
 module.exports = mongoose.connection;
-
-//  `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.cv4hyu6.mongodb.net/`,
